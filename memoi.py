3a09a7515b6c563ec5486300df1821c566c2e2ba
moimo233 <50516170+moi-mo@users.noreply.github.com>
2021-11-19 07:51:15 +0800

    add helpInfo; add shuffle cards-showing

--- memoi.py
+++ memoi.py
@@ -1,4 +1,5 @@
 # memoi by rmrfLab
+import random
 
 class Card:
     def __init__(self, front, back):
@@ -6,10 +7,23 @@ class Card:
         self.back = back
 
 cardL = []
-version = "0.01a"
-helpInfo = None
+version = "0.02a"
 goalAcc = 1
 exitCode = 0
+helpInfo = """
+'add cards' or 'adc'            Adding cards.
+'set gol accuracy' or 'sga'     Set a goal accuracy, which should be input in decimal
+'clear cards' or 'clc'          Delete all cards you've made
+'check cards' or 'ckc'          Show all the cards you've made
+'start memorizing' or 'smr'     Show cards in shuffle. We will first show a card's front
+                                and then waiting for a Enter before showing its back, t-
+                                hen, you can press Enter or whatever if your answer is
+                                right, or simply input a no.
+                                The shuffle will do again if your acuuracy is less than
+                                that you set as goal. The default goal accuracy is 1.
+'help'                          Show this help page.
+'quit'                          Quit the program.
+"""
 
 def main():
     print("memoi version "+version+" by rmrf lab!")
@@ -69,11 +83,13 @@ def memo():
     if n==0: print("[ You haven't made any cards yet! ]"); return
     wrong = n
     trueAcc = -1
+    tempL = cardL
+    random.shuffle(tempL)
     while trueAcc<goalAcc:
         for i in range(0, n):
-            print("[FRONT "+str(i)+": "+cardL[i].front+" ]", end='')
+            print("[FRONT "+str(i)+": "+tempL[i].front+" ]", end='')
             input()
-            print("[BACK "+str(i)+": "+cardL[i].back+" ]")
+            print("[BACK "+str(i)+": "+tempL[i].back+" ]")
             ans = input("[CORRECT?]\t\t")
             if ans!="no": wrong -= 1
         trueAcc = (n-wrong)/n
